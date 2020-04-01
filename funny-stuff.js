2f1cec9051d7f0562a9f60a17cbbca501150ccd3
Witek85 <witold.szklarski@gmail.com>
2020-04-01 23:48:19 +0200

    extend

--- funny-stuff.js
+++ funny-stuff.js
@@ -1,9 +1,18 @@
 (function($) {
     console.log('ok');
 
-    $.fn.funnyStuff = function() {
-        this.css( "background", "green" );
-        this.append( "<strong>lol</strong>" );
+    $.fn.funnyStuff = function(settings) {
+
+        var defaults = {
+            "color": "green",
+            "text": "test text lol"
+        }
+        
+        $.extend( defaults, settings );
+        console.log(defaults)
+
+        this.css( "background", defaults.color );
+        this.append( "<strong>" + defaults.text + "</strong>" );
     }
 
 })(jQuery);
